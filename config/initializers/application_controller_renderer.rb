92f2dc43f66827b7e140bab4f0b2c9dff5378e9d
SUZUKIYUI <iaguminano@docomo.ne.jp>
2021-02-24 13:51:15 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'b52b5307d709f118f91d891762fd59b7a736c637c7f1a05f3cc674fa224548bbdc68c6a25ef1051fad348aafb6f2305c933e50aa4a5b077a387c7b76af913011'
